9e064fcf2853a1999845901829257bffe44e5fa5
dede-sumarsono <dede.sumarsono45@gmail.com>
2023-04-26 21:52:41 +0700

    3. Get detail API

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostDetailResource;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -10,7 +12,17 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
-        return response()->json($posts);
+        //return response()->json(['data' => $posts]);
+
+        return PostResource::collection($posts);
         
+    }
+
+    public function show($id)
+    {
+        $post = Post::with('writer:id,username')->findOrFail($id);
+        
+        //return response()->json(['data' => $post]); 
+        return new PostDetailResource($post);
     }
 }
